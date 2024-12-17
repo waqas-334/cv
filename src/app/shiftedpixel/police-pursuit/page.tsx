4019a6f8bc99c177aa48d7d45f879e2af4069e1b
waqas-334 <waqasyounis334@gmail.com>
2024-12-17 11:10:42 -0330

    TEMP: redirecting everything to Google Play

--- src/app/shiftedpixel/police-pursuit/page.tsx
+++ src/app/shiftedpixel/police-pursuit/page.tsx
@@ -8,21 +8,38 @@ export default function PolicePursuit() {
   // const googlePackageName = "com.androidbull.incognito.browser";
   const appStoreLink =
     "https://apps.apple.com/us/app/police-pursuit-crash-em-all/id6739004227";
+  return (
+    <>
+      <GoogleAnalytics
+        eventName="redirected_to_android"
+        eventParams={{
+          page_title: "Android Redirect",
+          page_path: "/shiftedpixel/police-pursuit",
+        }}
+      />
+      {redirect(
+        `https://play.google.com/store/apps/details?id=${googlePackageName}`,
+      )}
+    </>
+  );
 
-  if (/android/i.test(userAgent)) {
-    <GoogleAnalytics
-      eventName="redirected_to_android"
-      eventParams={{
-        page_title: "Android Redirect",
-        page_path: "/shiftedpixel/police-pursuit",
-      }}
-    ></GoogleAnalytics>;
-    redirect(
-      `https://play.google.com/store/apps/details?id=${googlePackageName}`,
-    );
-  } else {
-    redirect(appStoreLink);
-  }
+  // if (/android/i.test(userAgent)) {
+  // <GoogleAnalytics
+  //   eventName="redirected_to_android"
+  //   eventParams={{
+  //     page_title: "Android Redirect",
+  //     page_path: "/shiftedpixel/police-pursuit",
+  //   }}
+  // ></GoogleAnalytics>;
+  // setTimeout(() => {
+  //   window.location.href = `https://play.google.com/store/apps/details?id=${googlePackageName}`;
+  //   // redirect(
+  //     // `https://play.google.com/store/apps/details?id=${googlePackageName}`,
+  //   // );
+  // }, 300);
+  // } else {
+  // redirect(appStoreLink);
+  // }
 
-  return null; // This will not render since the user is redirected
+  // return null; // This will not render since the user is redirected
 }
